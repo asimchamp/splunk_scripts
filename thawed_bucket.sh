9229bb7a1248063b92494ef1e1d0c6665384cf64
asimchamp <61060339+asimchamp@users.noreply.github.com>
2021-01-10 22:53:52 +0530

    update grep function

--- thawed_bucket.sh
+++ thawed_bucket.sh
@@ -1,8 +1,30 @@
 #! /bin/bash
 
-# set number of bucket rebuild in one time
+splunk_bucket_type_func()
+{
+CH2='Please enter your choice: '
+echo "$CH2"
+options1=("db" "rb" "All" "Quit")
+select bucket_type in "${options1[@]}"
+do
+    case "$bucket_type" in
+        "db")
+            break
+            ;;
+        "rb")
+            break
+            ;;
+        "All") 
+            break
+            ;;
+        "Quit")
+            exit
+            ;;
+        *) ;;
+    esac
+done
+}
 
-_maxChildProcs=1
 
 # read the index name
 echo "Please enter the index name :"
@@ -19,10 +41,13 @@ host=$(hostname)
 
 #full index restore_path
 
-thawed_path=(/Isilon-cold/forzen-temp/"$host"/"$index_name")
+thawed_path=(/Isilon-cold/forzen-temp/"$host"/"$index_name"/)
 
 echo "orignal path to copy bucket ="
 echo "$thawed_path"
+echo
+echo "Please Enter the Splunk Bucket Type :"
+splunk_bucket_type_func
 
 
 echo "Enter the start time (earliest_time) :(mm/dd/yyyy)"
@@ -43,12 +68,22 @@ echo "Latest time =""$latest_epoch"
 
 list_func()
 {
-bucket_id1=$(ls -d "$thawed_path"/* | grep db | awk -F"_" -v et="$earliest_epoch" -v lt="$latest_epoch", '$4>=et && $4<=lt{print $0"/"}')
-bucket_id2=$(ls -d "$thawed_path"/* | grep db | awk -F"_" -v et="$earliest_epoch" -v lt="$latest_epoch", '$5>=et && $5<=lt{print $0"/"}')
+
+if [[ "$bucket_type" == All ]];
+   then
+      bucket_id1=$(ls -d "$thawed_path"* | awk -F"/" '{print $6}' | awk -F"_" -v et="$earliest_epoch" -v lt="$latest_epoch", '$2>=et && $2<=lt{print $0"/"}')
+      bucket_id2=$(ls -d "$thawed_path"* | awk -F"/" '{print $6}' | awk -F"_" -v et="$earliest_epoch" -v lt="$latest_epoch", '$3>=et && $3<=lt{print $0"/"}')
+   else
+      bucket_id1=$(ls -d "$thawed_path"* | grep "$bucket_type" | awk -F"/" '{print $6}' | awk -F"_" -v et="$earliest_epoch" -v lt="$latest_epoch", '$2>=et && $2<=lt{print $0"/"}')
+      bucket_id2=$(ls -d "$thawed_path"* | grep "$bucket_type" | awk -F"/" '{print $6}' | awk -F"_" -v et="$earliest_epoch" -v lt="$latest_epoch", '$3>=et && $3<=lt{print $0"/"}')
+fi
+
 echo "$bucket_id1" > /Isilon-cold/thawedb/bucket.txt
 echo "$bucket_id2" >> /Isilon-cold/thawedb/bucket.txt
-sort bucket.txt | uniq > /Isilon-cold/thawedb/db_bucket.txt
-
+sort /Isilon-cold/thawedb/bucket.txt | uniq > /Isilon-cold/thawedb/db_bucket.txt
+bucket_id3=$(cat /Isilon-cold/thawedb/db_bucket.txt | awk -v th="$thawed_path", '{print th$0}')
+echo "$bucket_id3" > /Isilon-cold/thawedb/db_bucket.txt
+sed -i -e 's/,//' /Isilon-cold/thawedb/db_bucket.txt
 }
 
 index_present=$(ls /Isilon-cold/thawedb/ | grep "$index_name" | wc -l )
